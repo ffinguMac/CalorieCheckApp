4b5d988b0b0436961db510db600d7afa856964c4
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2024-11-13 18:53:12 +0900

    add logo Freepik

--- app/src/main/java/com/example/caloriecheckapp/MainActivity.java
+++ app/src/main/java/com/example/caloriecheckapp/MainActivity.java
@@ -1,24 +1,27 @@
 package com.example.caloriecheckapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Start 버튼 참조 및 클릭 리스너 설정
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 다음 화면으로 이동하는 인텐트 작성
+                Intent intent = new Intent(MainActivity.this, NextActivity.class); // NextActivity는 다음 화면의 액티비티
+                startActivity(intent);
+            }
         });
     }
 }
